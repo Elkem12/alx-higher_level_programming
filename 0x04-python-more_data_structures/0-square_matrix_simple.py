57eca536c000b1798c468371622f97c2b8974f0a
Niyi7 <emaniyi64@gmail.com>
2022-12-08 00:04:16 +0100

    Task 0 done

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,28 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+new_matrix = []
+row1 = []
+row2 = []
+row3 = []
+
+
+def square_matrix_simple(matrix=[]):
+    global row2, row1
+    new_matrix = matrix[:]
+
+    for row in range(0, len(new_matrix)):
+        for col in range(0, len(new_matrix[row])):
+            col_val = new_matrix[row][col]
+            col_val = col_val ** 2
+            row1.append(col_val)
+
+    for _ in range(0, len(row1)):
+        row2.append(row1[_])
+        if (_ + 1) % 3 == 0:
+            row3.append(row2)
+            row2 = []
+
+    return row3
