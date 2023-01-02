67fd74b18436a49de4df9d1044a2daa9c707b06a
JasmondWorks <obafemilared@gmail.com>
2023-01-02 22:35:29 +0100

    ALX higher level programming - All projects

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    row1 = []
-    row2 = []
-    row3 = []
-    
-    matrix2 = matrix[:]
-
-    for row in range(0, len(matrix2)):
-        for col in range(0, len(matrix2[row])):
-            col_val = matrix2[row][col]
-            col_val = col_val ** 2
-            row1.append(col_val)
-
-    for _ in range(0, len(row1)):
-        row2.append(row1[_])
-        if (_ + 1) % 3 == 0:
-            row3.append(row2)
-            row2 = []
-
-    return row3
+    tmp = []
+    for x in matrix:
+        tmp.append(list(map(lambda x: x**2, x)))
+    return (tmp)
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in new_list:
-        if i == search:
-            new_list[new_list.index(i)] = replace
-    return new_list
+    return (list(map(lambda x: replace if x is search else x, my_list)))
